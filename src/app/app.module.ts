15e3ea1bcf14c0e513ec3766706ac435f8e0d91e
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2023-03-01 23:00:44 +0100

    Passing data in Url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,14 @@ import { Routes } from '@angular/router';
 import {RouterModule} from '@angular/router'
 const appRoutes:Routes=[
   {path:'',component: HomeComponent},
-  {path:'user',component: UsersComponent},
-  {path:'user/:id/:name',component:UserComponent},
-  {path:'servers',component:ServersComponent},
-  {path:'servers/:id/edit',component:EditServerComponent}
+  {path:'user',component: UsersComponent,children:[
+    {path:':id/:name',component:UserComponent},
+  ]},
+  {path:'servers',component:ServersComponent,children:[
+    {path:':id/edit',component:EditServerComponent},
+    {path:':id',component:ServerComponent}
+  ]},
+
 ]
 @NgModule({
   declarations: [
